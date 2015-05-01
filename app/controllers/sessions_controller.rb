03a2525ca913342214925cabcd1c2af475f9144b
segfault <sf@sharped.net>
2015-05-01 23:34:05 +0200

    Added cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
+    
+    auth_uid = check_cookie.nil? ? auth_hash["uid"] : check_cookie
  
-    @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_uid)
     if @authorization
       render :text => "Welcome back #{@authorization.user.name}! You have already signed up. Are you enrolled? #{validate_enrollment(auth_hash["info"]["enrollments"])}"
     else
@@ -13,9 +15,15 @@ class SessionsController < ApplicationController
                       
       user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
       user.save
+      
+      cookies[:auth_uid] = { :value => auth_hash["uid"], :expires => Time.now + 1.hour}
  
       render :text => "Hi #{user.name}! You've signed up. Are you enrolled? #{validate_enrollment(auth_hash["info"]["enrollments"])}"
     end
+  end
+  
+  def check_cookie
+    auth_uid = cookies[:auth_uid]
   end
   
   def validate_enrollment(enrollments)
