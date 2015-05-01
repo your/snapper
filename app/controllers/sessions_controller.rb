45fb135c6708fd2c7b611eeb48912138037407a4
thatjoe <sf@sharped.net>
2015-05-01 10:00:10 -0400

    Gems added - first editings to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,12 @@ class SessionsController < ApplicationController
     if @authorization
       render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
     else
-      user = User.new :name => auth_hash["user_info"]["name"], :email => auth_hash["user_info"]["email"]
-      user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-      user.save
+#      user = User.new :name => auth_hash["user_info"]["name"]
+#, :email => auth_hash["user_info"]["email"]
+#      user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+#      user.save
  
-      render :text => "Hi #{user.name}! You've signed up."
+      render :text => "Hi #{auth_hash["info"]["name"]}! You've signed up."
     end
   end
 end
