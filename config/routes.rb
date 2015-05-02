5d1db16d59c027ef82e13c9355bbe72b1352b7c4
segfault <sf@sharped.net>
2015-05-02 17:00:45 +0200

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   
   #get '/auth/:provider/callback', to: 'sessions#create'
   get   '/login', :to => 'sessions#new', :as => :login
-  get '/auth/:provider/callback', :to => 'sessions#create' do
+  get '/auth/:provider/callback?:url', :to => 'sessions#create' do
     @url = params[:url]
     erb :url
   end
