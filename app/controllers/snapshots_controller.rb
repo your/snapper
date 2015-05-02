4cd36b9edc1449b523d504562160e01450ea91ca
segfault <sf@sharped.net>
2015-05-02 19:24:58 +0200

    Fixed: destroy session url as soon as used

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -3,7 +3,36 @@ class SnapshotsController < ApplicationController
     @snapshot = Snapshot.new
     session_url = session[:_url]
     if !session_url.nil?
-      p session_url
+      @snapshot.url = session_url
+      session[:_url].destroy
+      if check_cookies
+      
+        @authorization = Authorization.find_by_provider_and_uid("coursera", cookies[:_uid])
+      
+        if @authorization
+          @snapshot.user_id = @authorization.user_id
+        
+          if @snapshot.save
+            #
+            @snapshot.generated_hash = generate_hash(@snapshot.id)
+            @snapshot.save
+            #
+            flash[:snapshot_id] = @snapshot.id
+            redirect_to new_snapshot_url
+          else
+            render :new
+          end
+        
+        else
+          render :text => "wrong/expired auth"
+        end
+      
+      else
+        #render :text => "no/expired cookie"
+        session[:_url] = @snapshot.url
+        redirect_to :controller => 'sessions', :action => 'new'
+      end
+      
     end
   end
   
