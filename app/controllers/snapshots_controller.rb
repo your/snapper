5877ced05de8b784b0982dc2d62e922be7cfc4ef
segfault <sf@sharped.net>
2015-05-02 15:37:44 +0200

    Added redirect

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -11,26 +11,32 @@ class SnapshotsController < ApplicationController
     @snapshot = Snapshot.new(snapshot_params)
     
     if check_cookies
+      
       @authorization = Authorization.find_by_provider_and_uid("coursera", cookies[:_uid])
+      
       if @authorization
         @snapshot.user_id = @authorization.user_id
         
+        if @snapshot.save
+          #
+          @snapshot.generated_hash = generate_hash(@snapshot.id)
+          @snapshot.save
+          #
+          flash[:snapshot_id] = @snapshot.id
+          redirect_to new_snapshot_url
+        else
+          render :new
+        end
+        
+      else
+        render :text => "wrong/expired auth"
+      end
       
-    if @snapshot.save
-      #
-      @snapshot.generated_hash = generate_hash(@snapshot.id)
-      @snapshot.save
-      #
-      flash[:snapshot_id] = @snapshot.id
-      redirect_to new_snapshot_url
     else
-      render :new
+      render :text => "no/expired cookie"
+      redirect_to :controller => 'sessions', :action => 'create', :provider => 'coursera'
     end
     
-  end
-else
-  render :text => "lol"
-end
   end
   
   def generate_hash(url)
