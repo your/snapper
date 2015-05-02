032773082fc9d6516d6bfdab87b6ef7afdd8bd56
segfault <sf@sharped.net>
2015-05-02 19:20:03 +0200

    Fix

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -1,37 +1,9 @@
 class SnapshotsController < ApplicationController
   def new
-    url = session[:_url]
     @snapshot = Snapshot.new
-    if !url.nil?
-      @snapshot = Snapshot.new(:url => url)
-      #if check_cookies
-      
-        @authorization = Authorization.find_by_provider_and_uid("coursera", cookies[:_uid])
-      
-        if @authorization
-          @snapshot.user_id = @authorization.user_id
-        
-          if @snapshot.save
-            #
-            @snapshot.generated_hash = generate_hash(@snapshot.id)
-            @snapshot.save
-            #
-            flash[:snapshot_id] = @snapshot.id
-            redirect_to new_snapshot_url
-          else
-            render :new
-          end
-        
-        else
-          render :text => "wrong/expired auth"
-        end
-      
-        #else
-        #render :text => "no/expired cookie"
-        #session[:_url] = @snapshot.url
-        #redirect_to :controller => 'sessions', :action => 'new'
-        #end
-    end
+    session_url = session[:_url]
+    if !session_url.nil?
+      p session_url
   end
   
   def check_cookies
